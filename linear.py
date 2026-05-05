711614a9cf774cbf31fec4b45070b88b72d7075a
nima-2009 <nima20250466@miis.ae>
2026-05-05 14:04:34 +0400

    Update linear.py

--- linear.py
+++ linear.py
@@ -3,16 +3,16 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 df=pd.read_csv("student_scores")
-X=df[['Hours']]
-y=df[Scores']
-X+train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+X=df.iloc[:,:-1].values #features
+y=df.iloc[:,-1].values
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.2,random_state=42)
 model=LinearRegression()
-model.fit(X_train,y_train)
+model.fit(X_train,Y_train)
 st.title("exam score predictor")
 st.write("enter hours studied to predict exam score")
-hours=st.number_input("Hours:",min_value=0.0,step=0.1)
+hours=st.number_input("Hours studied:",min_value=0.0,step=0.1)
 if st.button("predict score"):
-  predicted score = model.predict([[hourse]])[0]
-  st.success(f"predicted score:{predicted_score:.2f}")
-  st.write("###sample training data")
-  st.dataframe(df)
+  predicted score = model.predict([[hours]])[0]
+  st.success(f"predicted score:{predicted_score: .2f}")
+st.write("sample training data")
+st.dataframe(df)
