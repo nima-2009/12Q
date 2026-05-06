b1cbc4de4b1c4b9acba6a990ffeb8971ec4f3acd
nima-2009 <nima20250466@miis.ae>
2026-05-06 09:53:20 +0400

    Update linear.py

--- linear.py
+++ linear.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-df=pd.read_csv("student_scores")
+df=pd.read_csv("student_scores.csv")
 X=df.iloc[:,:-1].values #features
 y=df.iloc[:,-1].values
 X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.2,random_state=42)
